6071cfdbba5e02a47bb2b965104b44f50f913a8b
Fadyy22 <fadyalaa441@gmail.com>
2023-10-05 21:53:46 +0300

    setup web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo 'Hello World!' > /data/web_static/releases/test/index.html
+echo 'Hello World!' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sh -c 'echo "server {
